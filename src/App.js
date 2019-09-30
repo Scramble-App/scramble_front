fec8a4c36dac0b34317c93c8e40126b6ae3e6c83
Victor <sexsportandrocknroll@gmail.com>
2019-09-30 21:50:23 +0300

    basic unstructured app with hardcoded companies list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss'
+
+import companies from './companies'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <div>
+        <h3>Filters</h3>
+        <ul>
+          <li>profitable</li>
+          <li>has black Jack</li>
+        </ul>
+        <button>All</button>
+        <button>Watchlist</button>
+      </div>
+      <div>
+        <h2>Companies list</h2>
+        <div className={styles.companiesList}>
+          {companies.map(company => (
+            <div className={styles.companyCard}>
+              {company.name}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
