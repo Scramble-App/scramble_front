bdac2fc5363e65675cb3c5fdb2e9e8dd900cc6e1
Victor <sexsportandrocknroll@gmail.com>
2019-10-03 16:14:37 +0300

    basic request paga

--- src/components/RequestsPage/index.js
+++ src/components/RequestsPage/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import styles from './RequestsPage.module.scss'
+import companies from '../../mockups/companies'
+import {Link} from "react-router-dom";
 
 const RequestsPage = () => {
   return (
-    <div>
-      Requests
+    <div className={styles.requestsPage}>
+      <div>
+
+      <h3>Filters:</h3>
+      <ul>
+        <li>Watch list</li>
+        <li>Swaps</li>
+      </ul>
+      </div>
+<div>
+  <div>
+    <p>25.09.2019</p>
+    <p>Watch list request:</p>
+    <p>Company: <Link to={`/companies/${companies[0].id}`}>{companies[0].name}</Link></p>
+    <p>Founder: {companies[0].founder}</p>
+    <button>Accept</button>
+    <button>Decline</button>
+  </div>
+  <div>
+    <p>24.09.2019</p>
+    <p>Watch list request:</p>
+    <p>Company: <Link to={`/companies/${companies[1].id}`}>{companies[1].name}</Link></p>
+    <p>Founder: {companies[1].founder}</p>
+    <button>Accept</button>
+    <button>Decline</button>
+  </div>
+</div>
     </div>
   )
 };
