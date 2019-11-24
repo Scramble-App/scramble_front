5182d54fb8e1098bfcbf69dc586e35cafde6e2c9
Victor <sexsportandrocknroll@gmail.com>
2019-11-24 19:51:43 +0300

    build

---
+++ build/precache-manifest.f9e1007c34c7e9a905383810506bdd1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed56224fbcfb1253cc9643020921451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30839d6970cb7e655099",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "8d212743c6fb160e0573",
+    "url": "/static/css/main.9fc9c62c.chunk.css"
+  },
+  {
+    "revision": "30839d6970cb7e655099",
+    "url": "/static/js/2.e33f4f2f.chunk.js"
+  },
+  {
+    "revision": "8d212743c6fb160e0573",
+    "url": "/static/js/main.6c9392d2.chunk.js"
+  },
+  {
+    "revision": "c623f6b7527ff9a1c5ac",
+    "url": "/static/js/runtime-main.31822bb0.js"
+  },
+  {
+    "revision": "14d305513afc8cbf642f04a7df4e43da",
+    "url": "/static/media/logo.14d30551.svg"
+  }
+]);
