ee3570d4cb65c0c390c25906b623b2f85982c643
Victor <sexsportandrocknroll@gmail.com>
2019-11-24 19:46:14 +0300

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98f45ffbff137de41623222bea08541f.js"
+  "/precache-manifest.f9e1007c34c7e9a905383810506bdd1c.js"
 );
 
 self.addEventListener('message', (event) => {
