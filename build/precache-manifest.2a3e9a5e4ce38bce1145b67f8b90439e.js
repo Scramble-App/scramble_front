7bea052576ac8687de0e470f98b195242a337243
Victor <sexsportandrocknroll@gmail.com>
2020-02-12 17:39:48 +0300

    build

---
+++ build/precache-manifest.2a3e9a5e4ce38bce1145b67f8b90439e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a4d4f882f5d27c8a65fd994a295b5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c854eb0b921176ba0350",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "f44fe0c1ea48e3837d9b",
+    "url": "/static/css/main.1d84baf4.chunk.css"
+  },
+  {
+    "revision": "c854eb0b921176ba0350",
+    "url": "/static/js/2.704e7745.chunk.js"
+  },
+  {
+    "revision": "f44fe0c1ea48e3837d9b",
+    "url": "/static/js/main.121e8374.chunk.js"
+  },
+  {
+    "revision": "c623f6b7527ff9a1c5ac",
+    "url": "/static/js/runtime-main.31822bb0.js"
+  },
+  {
+    "revision": "14d305513afc8cbf642f04a7df4e43da",
+    "url": "/static/media/logo.14d30551.svg"
+  },
+  {
+    "revision": "f688d6ca27785b6b3028c590be3c3cb0",
+    "url": "/static/media/logo_white.f688d6ca.svg"
+  }
+]);
