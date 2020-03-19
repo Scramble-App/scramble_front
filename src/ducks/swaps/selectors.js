2042d20fa739b667acff0266afdfe00c69fae5d6
Victor <sexsportandrocknroll@gmail.com>
2020-03-19 21:25:45 +0300

    swap requests from active fundraising only

--- src/ducks/swaps/selectors.js
+++ src/ducks/swaps/selectors.js
@@ -4,35 +4,6 @@ import {companiesListSelector, ownCompanyActiveFundraisingSelector, ownCompanySe
 // TODO fix type hardcode
 export const swapsSelector = state => state.swaps.map(swap => ({...swap}))
 
-// TODO might need to change to ownSwaps only?
-export const outcomeSwapsSelector = createSelector(
-  [swapsSelector, companiesListSelector, ownCompanySelector],
-  (swaps, companies, ownCompany) => (
-    swaps
-      .filter(swap => swap.sender === ownCompany.id)
-      .map((swap) => ({
-        ...swap,
-        sender: companies.find(company => company.id === swap.sender),
-        target: companies.find(company => company.id === swap.target),
-        subtype: 'outcome'
-      }))
-  )
-)
-
-export const incomeSwapsSelector = createSelector(
-  [swapsSelector, companiesListSelector, ownCompanySelector],
-  (swaps, companies, ownCompany) => (
-    swaps
-      .filter(swap => swap.target === ownCompany.id)
-      .map((swap) => ({
-        ...swap,
-        sender: companies.find(company => company.id === swap.sender),
-        target: companies.find(company => company.id === swap.target),
-        subtype: 'income'
-      }))
-  )
-)
-
 export const companySwapsSelector = createSelector(
   [companiesListSelector, ownCompanyActiveFundraisingSelector],
   (companies, activeFundraising) => {
@@ -48,3 +19,33 @@ export const companySwapsSelector = createSelector(
     )
   }
 )
+
+// TODO might need to change to ownSwaps only?
+export const outcomeSwapsSelector = createSelector(
+  [companySwapsSelector, companiesListSelector, ownCompanySelector],
+  (swaps, companies, ownCompany) => (
+    swaps
+      .filter(swap => swap.sender === ownCompany.id)
+      .map((swap) => ({
+        ...swap,
+        sender: companies.find(company => company.id === swap.sender),
+        target: companies.find(company => company.id === swap.target),
+        subtype: 'outcome'
+      }))
+  )
+)
+
+export const incomeSwapsSelector = createSelector(
+  [companySwapsSelector, companiesListSelector, ownCompanySelector],
+  (swaps, companies, ownCompany) => (
+    swaps
+      .filter(swap => swap.target === ownCompany.id)
+      .map((swap) => ({
+        ...swap,
+        sender: companies.find(company => company.id === swap.sender),
+        target: companies.find(company => company.id === swap.target),
+        subtype: 'income'
+      }))
+  )
+)
+
