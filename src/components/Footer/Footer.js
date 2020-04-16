a93268999a91290cfef252d9546c4b8c612ec6e6
Nurbek Avtandilov <avtandilov06@gmail.com>
2020-04-16 23:10:34 +0600

    implement new version of footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import styles from './Footer.module.scss'
-import {Icon} from "antd";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <div className={styles.wrapper}>
-        <p><Icon type="mail" /> info@scrambleup.com</p>
-        <p>© Scramble, 2019</p>
+    <footer className={styles.footer}>
+      <div className={styles.logo}>
+        <i className={classNames(styles.scramble, styles.scrambleLogo)}/>
       </div>
-    </div>
+      <div className={styles.copyright}>&copy; 2020 scramble. All rights reserved</div>
+      <div className={styles.address}>950 Page Mill Road, Palo Alto, CA 94304, USA</div>
+      <ul className={styles.social}>
+        <li>
+          <a href="https://www.facebook.com/ScrambleUpCom/" target="_blank" rel="noopener">
+            <i className={classNames(styles.scramble, styles.scrambleFacebook)}/>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/company/scrambleup" target="_blank" rel="noopener">
+            <i className={classNames(styles.scramble, styles.scrambleLinkedin)}/>
+          </a>
+        </li>
+      </ul>
+    </footer>
   );
 };
 
